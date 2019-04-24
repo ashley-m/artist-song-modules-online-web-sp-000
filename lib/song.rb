8118eb1a8ce1653593b348808763ec369a91a3b7
Ashley Miller <asm2fe@virginia.edu>
2019-04-24 20:12:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
 
   extend Memorable
   extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -26,7 +27,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
